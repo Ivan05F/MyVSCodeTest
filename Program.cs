5582812532f0cbe910446fc2360dd05edc4d327d
Ivan05F <sistemasweb.cf@gmail.com>
2017-10-05 21:20:58 -0500

    Conversion a proyecto .NET

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyVSCodeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello mundo!");
+        }
+    }
+}
